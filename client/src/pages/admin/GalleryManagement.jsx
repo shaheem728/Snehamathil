c188fcf9d27830d7fd269bc3751c9e60b943c771
shaheem <shaheemmahmood728@gmail.com>
2025-11-10 15:23:23 +0530

    Update GalleryManagement.jsx: add className to div and input

--- client/src/pages/admin/GalleryManagement.jsx
+++ client/src/pages/admin/GalleryManagement.jsx
@@ -267,13 +267,13 @@ const deleteImage = async (id) => {
                 className="w-full border  border-gray-300 rounded-md px-3 py-2 focus:ring-2 focus:ring-[#204E43] outline-none"></textarea>
               </div>
 
-              <div>
+              <div className="flex flex-col">
                 <label className="text-sm text-gray-700">Display Order</label>
                 <input
                   type="number"
                   value={order}
                   onChange={(e) => setOrder(e.target.value)}
-                  className="w-full border border-gray-300 rounded-md px-3 py-2 focus:ring-2 focus:ring-[#204E43] outline-none"
+                  className="w-1/2 border border-gray-300 rounded-md px-3 py-2 focus:ring-2 focus:ring-[#204E43] outline-none"
                 />
               </div>
 
